0963736404057df5f77766c77694b443a8581f8c
oje-edu <Thorsten.Oje@gfn.education>
2020-12-18 21:58:16 +0100

    initital
    next l06c vorbereitungskurs

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 64],
+    domains: ["images.unsplash.com", "pexels.com", "images.ctfassets.net"],
+    path: "/_next/image",
+    loader: "default",
+  },
+};
